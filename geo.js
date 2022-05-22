8811e0829e2cac0764304278179c567422e74db6
AnataKyoya <48954955+AnataKyoya@users.noreply.github.com>
2022-05-22 23:53:44 +0800

    all
    all

--- geo.js
+++ geo.js
@@ -1,15 +1,21 @@
-var x = document.getElementById("demo");
-var a = document.querySelector(".alert");
+var c = document.querySelector(".coordinate");
+var id = document.querySelector(".id");
+
+getLocation();
 
 function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
     } else {
-        x.innerHTML = "Geolocation is not supported by this browser.";
+        id.innerHTML = "Geolocation is not supported by this browser.";
     }
-}
+};
 
 function showPosition(position) {
-    a.style = "display: block";
-    x.innerHTML = "Your Id: " + position.coords.latitude + " (" + position.coords.longitude + ")";
-}
+    c.innerHTML = position.coords.latitude + ", " + position.coords.longitude;
+    id.innerHTML = "Id : ask12n2r3coj23eewoj12f";
+};
+
+setTimeout(function () {
+    id.innerHTML = "Id : lkrf11nke3oejfojrr35weo";
+}, 10000);
